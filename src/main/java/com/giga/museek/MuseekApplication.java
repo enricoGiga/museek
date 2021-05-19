6b671bf861a844c2c8892e23343013a3e20b0aac
enrico_gigante <enrico.gigante@gmail.com>
2021-05-19 12:13:06 +0200

    refactor

---
+++ src/main/java/com/giga/museek/MuseekApplication.java
@@ -0,0 +1,13 @@
+package com.giga.museek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuseekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/giga/museek/MuseekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giga.museek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
